c3552466081be16e47e6c91ebfad07c46caaea07
Skyth9 <dobri.kirkov@gmail.com>
2025-05-30 16:57:22 +0300

    adding providers and vars

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.27.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+  subscription_id    = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
+  client_id          = "c1626f24-bf57-4f50-aa5e-10d1aa1180bf"
+  client_secret      = "X2i8Q~a0nkHAmRnqNKDipG_CdtLaazQOoBJ3YczH"
+  tenant_id          = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
+}
