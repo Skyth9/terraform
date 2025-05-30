f9f2f12210f184e4dde8c03dcea1fd17e0d9f5cd
Skyth9 <dobri.kirkov@gmail.com>
2025-05-30 17:04:51 +0300

    offff

--- provider.tf
+++ provider.tf
@@ -13,5 +13,5 @@ provider "azurerm" {
   subscription_id    = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
   client_id          = "c1626f24-bf57-4f50-aa5e-10d1aa1180bf"
   client_secret      = "X2i8Q~a0nkHAmRnqNKDipG_CdtLaazQOoBJ3YczH"
-  tenant_id          = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
+  tenant_id          = ""
 }
