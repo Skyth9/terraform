e5ab20bd82b2a42c9aae92c68240426e19eb0134
Skyth9 <dobri.kirkov@gmail.com>
2025-06-02 10:29:12 +0300

    secret2

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
-variable "client_secret" {
-}
+//variable "client_secret" {
+//}
 
 terraform {
   required_providers {
@@ -15,6 +15,6 @@ provider "azurerm" {
   resource_provider_registrations = "none"
   subscription_id    = "2213e8b1-dbc7-4d54-8aff-b5e315df5e5b"
   client_id          = "3d9e9bd5-be9d-4eb3-8caa-877439cd88c3"
-  client_secret      = var.client_secret
+  client_secret      = "ZgG8Q~5Bpm-ZStlNtrcRCP5e2JCWidxpD_moTaJb"
   tenant_id          = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
 }
