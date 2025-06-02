2e7a21e787738494161e72064ee077e660ab8560
Skyth9 <dobri.kirkov@gmail.com>
2025-06-02 10:39:24 +0300

    as

--- provider.tf
+++ provider.tf
@@ -15,6 +15,6 @@ provider "azurerm" {
   resource_provider_registrations = "none"
   subscription_id    = "2213e8b1-dbc7-4d54-8aff-b5e315df5e5b"
   client_id          = "3d9e9bd5-be9d-4eb3-8caa-877439cd88c3"
-  client_secret      = "ZgG8Q~5Bpm-ZStlNtrcRCP5e2JCWidxpD_moTaJb"
+  client_secret      = ${{ secrets.SuperSecret }}
   tenant_id          = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
 }
